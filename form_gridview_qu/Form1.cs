3ce4cdae4670ee18f8c9ac9e4df682a49b82acfc
jasaga2401 <jasaga@protonmail.com>
2023-11-22 16:02:29 +0000

    Add project files.

---
+++ form_gridview_qu/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_gridview_qu
+{
+    public partial class Form1 : Form
+    {
+        // Connection string for your SQL Server database
+        private string connectionString = "Data Source=Amaze\\SQLEXPRESS;Initial Catalog=things_to_do;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load data on form load
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // SQL Query
+                    string query = "SELECT * FROM dbo.tbl_customer";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        // Create a DataTable to hold the results
+                        DataTable dataTable = new DataTable();
+
+                        // Use SqlDataAdapter to fill the DataTable
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            adapter.Fill(dataTable);
+                        }
+
+                        // Bind the DataTable to the DataGridView
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Refresh the data when the button is clicked
+            LoadData();
+        }
+    }
+}
+
+
+
